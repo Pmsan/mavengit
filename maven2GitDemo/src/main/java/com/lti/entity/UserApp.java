8da06392f64356c0be924363a61794823cfd6c11
Pmsam <nitishsingh1300@gmail.com>
2022-07-14 12:26:03 +0530

    Second commit

--- maven2GitDemo/src/main/java/com/lti/entity/UserApp.java
+++ maven2GitDemo/src/main/java/com/lti/entity/UserApp.java
@@ -4,7 +4,7 @@ public class UserApp {
 public static void main(String[] args) {
 	Integer.parseInt("One");
 	calculator c = new calculator();
-	int r=c.add(100,300);
+	int r=c.add(1000,3000);
 	System.out.println(r);
 }
 }
